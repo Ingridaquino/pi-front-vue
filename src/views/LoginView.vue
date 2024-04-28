c578bc979c5e16766cb5a36f08fcca916499d705
ingrid Aquino <ingridaquinosoares@live.com>
2024-04-27 21:25:36 -0300

    iniciando projeto

---
+++ src/views/LoginView.vue
@@ -0,0 +1,28 @@
+<template>
+   <div class="container">
+    <div class="container__logo">logo</div>
+
+    <div class="container__box">
+        <form @submit.prevent="login">
+            <input />
+        </form>
+    </div>
+
+   </div>
+</template>
+<script setup>
+function login(){
+    
+}
+</script>
+<style>
+.container {
+    display: grid;
+    grid-template-columns: 1fr 400px;
+    width: 100%;
+    background-color: red;
+
+}
+
+
+</style>
